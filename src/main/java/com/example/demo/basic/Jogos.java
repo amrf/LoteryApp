7a0f9da0ca753a853ab98eb78d2d4802bfd546ac
Alfredo Rangel <alfredo.rangel@outlook.com>
2018-02-10 01:38:53 -0300

    Alteracoes

---
+++ src/main/java/com/example/demo/basic/Jogos.java
@@ -0,0 +1,17 @@
+package com.example.demo.basic;
+
+import java.util.TreeSet;
+
+public class Jogos {
+
+	TreeSet<Integer> treeseet;
+
+	public TreeSet<Integer> getTreeseet() {
+		return treeseet;
+	}
+
+	public void setTreeseet(TreeSet<Integer> treeseet) {
+		this.treeseet = treeseet;
+	}
+
+}
