b6fe323dcad772f554900ce1709a92e674e04371
Alfredo Rangel <alfredo.rangel@outlook.com>
2018-02-09 21:13:40 -0300

    Criacao do contrller

--- src/main/java/com/example/demo/controller/LoteryController.java
+++ src/main/java/com/example/demo/controller/LoteryController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoteryController {
 
 	
-	@RequestMapping(method = RequestMethod.POST)
-	void readBookmarks() {
+	@RequestMapping(method = RequestMethod.GET)
+	String readBookmarks() {
 		System.out.println("Rodou!!");
+		return "rodou";
 	}
 }
