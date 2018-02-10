5eb58b5e9a3fb287dc88ab3645c2661400565433
Alfredo Rangel <alfredo.rangel@outlook.com>
2018-02-10 00:53:49 -0300

    Criacao do contrller

--- src/main/java/com/example/demo/controller/LoteryController.java
+++ src/main/java/com/example/demo/controller/LoteryController.java
@@ -1,17 +1,58 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.basic.LoteryCheck;
+
 @RestController
-@RequestMapping("/lotery")
+@RequestMapping("/")
 public class LoteryController {
 
-	
 	@RequestMapping(method = RequestMethod.GET)
-	String readBookmarks() {
-		System.out.println("Rodou!!");
-		return "rodou";
+	public @ResponseBody LoteryCheck readBookmarks() {
+
+		TreeSet<Integer> lastNumbers = null;
+		try {
+			lastNumbers = readPage();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new LoteryCheck("Nós Ganhamos!!", lastNumbers);
+
 	}
+
+	public TreeSet<Integer> readPage() throws Exception {
+		TreeSet<Integer> lastNumbers = new TreeSet<Integer>();
+		Document doc = Jsoup.connect("https://noticias.uol.com.br/loterias/mega-sena/").get();
+		Elements content = doc.getElementsByClass("jogo");
+		Element headline = content.first();
+
+		Elements content1 = headline.getElementsByTag("td");
+		for (Element element : content1) {
+
+			String number = new String(element.toString());
+			number = number.replaceFirst("<td>", "");
+			number = number.replaceFirst("</td>", "");
+			lastNumbers.add(Integer.valueOf(number));
+		}
+
+		return lastNumbers;
+
+	}
+
 }
